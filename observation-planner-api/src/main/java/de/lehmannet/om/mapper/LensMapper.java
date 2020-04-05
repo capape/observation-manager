f7c0d3315e31bf8669a0d226ddafc0ed76e4df8f
Jose Antonio Capape Gil <capapegil@gmail.com>
2020-04-05 12:58:02 +0200

    Refactor lens methods too long

---
+++ observation-planner-api/src/main/java/de/lehmannet/om/mapper/LensMapper.java
@@ -0,0 +1,120 @@
+package de.lehmannet.om.mapper;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import de.lehmannet.om.IEquipment;
+import de.lehmannet.om.IImager;
+import de.lehmannet.om.ILens;
+import de.lehmannet.om.ISchemaElement;
+import de.lehmannet.om.util.FloatUtil;
+import de.lehmannet.om.util.SchemaException;
+
+public class LensMapper {
+
+    public static String getOptionalVendor(Element lensElement) throws SchemaException {
+        NodeList children;
+        Element child;
+        // Get optional vendor
+        child = null;
+        children = lensElement.getElementsByTagName(ILens.XML_ELEMENT_VENDOR);
+        StringBuilder vendor = new StringBuilder();
+        if (children != null) {
+            if (children.getLength() == 1) {
+                child = (Element) children.item(0);
+                if (child != null) {
+                    NodeList textElements = child.getChildNodes();
+                    if ((textElements != null) && (textElements.getLength() > 0)) {
+                        for (int te = 0; te < textElements.getLength(); te++) {
+                            vendor.append(textElements.item(te).getNodeValue());
+                        }
+                        return vendor.toString();
+                    }
+                    /*
+                     * vendor = child.getFirstChild().getNodeValue(); if( vendor != null ) {
+                     * this.setVendor(vendor); }
+                     */
+                } else {
+                    throw new SchemaException("Problem while retrieving vendor from lens. ");
+                }
+            } else if (children.getLength() > 1) {
+                throw new SchemaException("Lens can have only one vendor. ");
+            }
+        }
+        return null;
+    }
+
+    public static boolean getOptionalAvailability(Element lensElement) {
+        // Search for optional availability comment within nodes
+        NodeList list = lensElement.getChildNodes();
+        for (int i = 0; i < list.getLength(); i++) {
+            Node c = list.item(i);
+            if (c.getNodeType() == Node.COMMENT_NODE) {
+                if (IEquipment.XML_COMMENT_ELEMENT_NOLONGERAVAILABLE.equals(c.getNodeValue())) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static float getMandatoryFactor(Element lensElement) throws SchemaException {
+        NodeList children;
+        Element child;
+        // Get mandatory factor
+        child = null;
+        children = lensElement.getElementsByTagName(ILens.XML_ELEMENT_FACTOR);
+        if ((children == null) || (children.getLength() != 1)) {
+            throw new SchemaException("Lens must have exact one focal length factor. ");
+        }
+        child = (Element) children.item(0);
+        String factor = null;
+        if (child == null) {
+            throw new SchemaException("Lens must have a focal length factor. ");
+        } else {
+            factor = child.getFirstChild().getNodeValue();
+        }
+        return FloatUtil.parseFloat(factor);
+     
+    }
+
+    public static String getMandatoryModel(Element lensElement) throws SchemaException {
+        NodeList children;
+        Element child;
+        // Get mandatory model
+        children = lensElement.getElementsByTagName(ILens.XML_ELEMENT_MODEL);
+        if ((children == null) || (children.getLength() != 1)) {
+            throw new SchemaException("Lens must have exact one model name. ");
+        }
+        child = (Element) children.item(0);
+        StringBuilder model = new StringBuilder();
+        if (child == null) {
+            throw new SchemaException("Lens must have a model name. ");
+        } else {
+            if (child.getFirstChild() != null) {
+                NodeList textElements = child.getChildNodes();
+                if ((textElements != null) && (textElements.getLength() > 0)) {
+                    for (int te = 0; te < textElements.getLength(); te++) {
+                        model.append(textElements.item(te).getNodeValue());
+                    }
+                    return model.toString();
+                }
+                // model = child.getFirstChild().getNodeValue();
+            } else {
+                throw new SchemaException("Lens cannot have an empty model name. ");
+            }
+        }
+        return null;
+    }
+
+    public static String getMandatoryID(Element lensElement) throws SchemaException {
+        // Get ID from element
+        String ID = lensElement.getAttribute(ISchemaElement.XML_ELEMENT_ATTRIBUTE_ID);
+        if ((ID != null) && ("".equals(ID.trim()))) {
+            throw new SchemaException("Lens must have a ID. ");
+        }
+        return ID;
+    }
+
+}
