6bb59308f8e3d33f9d6b92c1fbfbdeb84e678f6e
Jose Antonio Capape Gil <capapegil@gmail.com>
2020-04-27 18:45:54 +0200

    Added theme manager to night vision

---
+++ observation-manager/src/main/java/de/lehmannet/om/ui/theme/ThemeManagerImpl.java
@@ -0,0 +1,147 @@
+package de.lehmannet.om.ui.theme;
+
+import java.lang.reflect.Constructor;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.MetalTheme;
+
+import de.lehmannet.om.ui.navigation.ObservationManager;
+import de.lehmannet.om.ui.util.IConfiguration;
+
+public class ThemeManagerImpl implements ThemeManager {
+
+    private boolean nightVision = false;
+    private final ObservationManager observationManager;
+    private final IConfiguration configuration;
+
+    private static NightVisionTheme NIGHT_THEME = new NightVisionTheme();
+
+    public ThemeManagerImpl(IConfiguration config, ObservationManager om) {
+        this.observationManager = om;
+        this.configuration = config;
+    }
+
+    @Override
+    public boolean isNightVision() {
+        // TODO Auto-generated method stub
+        return this.nightVision;
+    }
+
+    @Override
+    public void setTheme(String theme) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void enableNightVision() {
+        this.nightVision = true;
+        try {
+            // Check for Metal LAF
+            LookAndFeelInfo[] laf = UIManager.getInstalledLookAndFeels();
+            boolean found = false;
+            for (LookAndFeelInfo lookAndFeelInfo : laf) {
+                if ("metal".equals(lookAndFeelInfo.getName().toLowerCase())) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                System.err.println(ObservationManager.bundle.getString("error.noMetalLAF"));
+                this.observationManager.createWarning(ObservationManager.bundle.getString("error.noNightVision"));
+                return;
+            }
+
+            // Try to load MetalLookAndFeel
+            MetalLookAndFeel.setCurrentTheme(NIGHT_THEME);
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(this.observationManager);
+
+            // Make all frames and dialogs use the LookAndFeel
+
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            this.observationManager.dispose();
+            this.observationManager.setUndecorated(true);
+            this.observationManager.addNotify();
+            this.observationManager.createBufferStrategy(2);
+            this.observationManager.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+            this.observationManager.update(this.observationManager.getGraphics());
+            this.configuration.setConfig(ObservationManager.CONFIG_NIGHTVISION_ENABLED, Boolean.toString(true));
+            this.observationManager.setVisible(true);
+
+        } catch (Exception e) {
+            System.err.println(e);
+            this.observationManager.createWarning(ObservationManager.bundle.getString("error.noNightVision"));
+        }
+
+    }
+
+    @Override
+    public void disableNightVision() {
+        this.nightVision = false;
+        try {
+
+            // Try to load (default) OceanThema (available since Java 1.5)
+            // with relfection
+            Class<?> themeClass = null;
+            try {
+                themeClass = ClassLoader.getSystemClassLoader().loadClass("javax.swing.plaf.metal.OceanTheme");
+            } catch (ClassNotFoundException cnfe) {
+                // Can do nothing in here...defaultMetalTheme will be
+                // loaded...
+            }
+
+            boolean problem = true;
+            if (themeClass != null) { // Check if load OceanTheme succeeded
+                Constructor<?>[] constructors = themeClass.getConstructors();
+                if (constructors.length > 0) {
+
+                    for (Constructor<?> constructor : constructors) {
+                        if (constructor.getParameterTypes().length == 0) { // Use default
+                            // constructor and
+                            // set theme
+                            MetalTheme theme = (MetalTheme) constructor.newInstance();
+                            MetalLookAndFeel.setCurrentTheme(theme);
+                            problem = false; // No problem -> no need to
+                            // load DefaultMetalTheme as
+                            // we can use OceanTheme
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (problem) { // Ocean Theme cannot be used for whatever reason
+                MetalLookAndFeel.setCurrentTheme(new DefaultMetalTheme());
+            }
+
+            UIManager.setLookAndFeel(new MetalLookAndFeel());
+            SwingUtilities.updateComponentTreeUI(this.observationManager);
+
+            // Make all frames and dialogs use the LookAndFeel
+            JFrame.setDefaultLookAndFeelDecorated(false);
+            JDialog.setDefaultLookAndFeelDecorated(false);
+            this.observationManager.dispose();
+            this.observationManager.setUndecorated(false);
+            this.observationManager.addNotify();
+            this.observationManager.createBufferStrategy(2);
+            this.observationManager.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+            this.observationManager.update(this.observationManager.getGraphics());
+            this.configuration.setConfig(ObservationManager.CONFIG_NIGHTVISION_ENABLED, Boolean.toString(false));
+            this.observationManager.setVisible(true);
+
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+}
