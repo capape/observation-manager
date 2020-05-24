20df1083da30fd7992a4d0d007e29e7bdeeb5654
Jose Antonio Capape Gil <capapegil@gmail.com>
2020-05-24 18:35:45 +0200

    Delete kk folder

--- observation-manager/observation-manager-distribution/kk/README.md
+++
@@ -1,4 +0,0 @@
-# observation-manager
-Fork from Observation Manager https://sourceforge.net/projects/observation/
-
-The source code has been checkout from CVS and imported into git
--- observation-manager/observation-manager-distribution/kk/obs.bat
+++
@@ -1,7 +0,0 @@
-echo Starting ObservationManager
-
-:: Going to batch files dir
-cd %0\..\
-
-:. Starting up Observation Manager
-start javaw -Dextensions.dir="$DIR"/extensions -cp classpath:classpath/observation-manager-jar-with-dependencies.jar de.lehmannet.om.ObservationManagerApp %1 %2 %3 %4 %5 %6 %7 %8 %9
