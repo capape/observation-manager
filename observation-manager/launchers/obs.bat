c5b41f7231f4835d90ee3f79ed7f7453b1abfb9e
Jose Antonio Capape Gil <capapegil@gmail.com>
2020-05-24 16:41:33 +0200

    Use UTC dates

--- observation-manager/launchers/obs.bat
+++ observation-manager/launchers/obs.bat
@@ -4,4 +4,4 @@ echo Starting ObservationManager
 cd %0\..\
 
 :. Starting up Observation Manager
-start javaw -Dextensions.dir="$DIR"/extensions -cp classpath:classpath/observation-manager-jar-with-dependencies.jar de.lehmannet.om.ObservationManagerApp %1 %2 %3 %4 %5 %6 %7 %8 %9
+start javaw -Duser.timezone=UTC -Dextensions.dir="$DIR"/extensions -cp classpath:classpath/observation-manager-jar-with-dependencies.jar de.lehmannet.om.ObservationManagerApp %1 %2 %3 %4 %5 %6 %7 %8 %9
