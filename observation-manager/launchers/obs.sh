01624107df0b5abe3ce4ee135245b5e2fac44c7a
Jose Antonio Capape Gil <capapegil@gmail.com>
2020-05-25 17:30:14 +0200

    Fix error command at eof

--- observation-manager/launchers/obs.sh
+++ observation-manager/launchers/obs.sh
@@ -8,4 +8,3 @@ cd $DIR
 
 # Starting up Observation Manager
 java -Duser.timezone=UTC -Dextensions.dir="$DIR"/extensions -cp classpath:classpath/observation-manager-jar-with-dependencies.jar de.lehmannet.om.ObservationManagerApp
-gist
