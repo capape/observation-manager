07f3f17c658711206e7315dc45badf6603a762e6
Jose Antonio Capape Gil <capapegil@gmail.com>
2023-06-13 06:32:50 +0200

    Spotbug fixes

--- observation-manager/observation-planner-api/src/main/java/de/lehmannet/om/Angle.java
+++ observation-manager/observation-planner-api/src/main/java/de/lehmannet/om/Angle.java
@@ -117,9 +117,6 @@ public class Angle implements Serializable {
     }
 
     public static Angle of(Angle angle) {
-        if (angle == null) {
-            return null;
-        }
         return new Angle(angle.getValue(), angle.getUnit());
     }
 
